5c1ebb5d128fdd391b710da410cd18ecef940ae7
Pushpendra verma <pv842984@gmail.com>
2024-08-12 03:59:49 +0530

    Just checking Because of issue

--- Searching/BinarySearch/Binary_Search.java
+++ Searching/BinarySearch/Binary_Search.java
@@ -2,7 +2,8 @@ package Searching.BinarySearch;
 
 public class Binary_Search {
     public static void main(String[] args) {
-        int[] arr = { 12, 6, 9, 24, 6, 49, 11 };
+        int[] nums = { 12, 6, 9, 24, 6, 49, 11 };
+
     }
 
 }
